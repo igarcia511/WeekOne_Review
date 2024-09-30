799a9aaf4a459af491cfafc27180cd826433e7be
Ivan Garcia <iegarcia611@gmail.com>
2024-09-30 11:21:41 -0500

    Added subtract method

--- src/main/java/com/ps/Calculator.java
+++ src/main/java/com/ps/Calculator.java
@@ -4,9 +4,16 @@ class Calculator {
     public static void main(String[] args) {
        int sum = add(1,3);
         System.out.println(sum);
+        int subtract = subtract(5, 3);
+        System.out.println(subtract);
     }
     //static method add
     public static int add(int num1, int num2){
         return  num1 + num2;
     }
+    //static subtract method
+    public static int subtract(int num1, int num2){
+        return num1 - num2;
+    }
+    // static
 }
