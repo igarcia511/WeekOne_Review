174876c287cba83e224d11630713b00235fa3171
Ivan Garcia <iegarcia611@gmail.com>
2024-09-30 11:07:19 -0500

    created addition method

--- src/main/java/com/ps/Calculator.java
+++ src/main/java/com/ps/Calculator.java
@@ -2,6 +2,11 @@ package com.ps;
 
 class Calculator {
     public static void main(String[] args) {
-
+       int sum = add(1,3);
+        System.out.println(sum);
+    }
+    //static method add
+    public static int add(int num1, int num2){
+        return  num1 + num2;
     }
 }
